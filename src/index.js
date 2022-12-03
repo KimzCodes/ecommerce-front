d10ee1ffecb8aab435923b116cc90282114c451d
kareem nour <kimzcodes@gmail.com>
2022-12-03 17:46:26 +0200

    create routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import Items from "./pages/Items";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/categories",
+        element: <Categories />,
+      },
+      {
+        path: "/categories/:id/items",
+        element: <Items />,
+        loader: ({ params }) => {
+          if (isNaN(params.id)) {
+            throw new Response("Bad Request", {
+              statusText: "Item not found",
+              status: 400,
+            });
+          }
+        },
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+import { Button, Container } from "react-bootstrap";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <Container>
+      <div className="notFound">
+        <h1>{error.status}</h1>
+        <p>{error.statusText}</p>
+
+        <Button variant="link">Go Back</Button>
+      </div>
+    </Container>
+  );
+};
+
+export default ErrorPage;
