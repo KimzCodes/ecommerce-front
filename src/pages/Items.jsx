a3d25dc0a86655499c8340b4da505e7254b25e7a
kareem nour <kimzcodes@gmail.com>
2022-12-11 19:39:09 +0200

    items and filter

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { filterItems } from "../state/ItemSlice";
+import { useParams } from "react-router-dom";
+import { Item } from "../components/ecom-ui";
+import { GridList } from "../components/Layout";
 
 const Items = () => {
-  return <div>Items</div>;
+  const dispatch = useDispatch();
+  const { prefix } = useParams();
+
+  const { loading, error, records } = useSelector((state) => state.items);
+  console.log(records);
+  useEffect(() => {
+    dispatch(filterItems(prefix));
+  }, [dispatch, prefix]);
+
+  return (
+    <div>
+      <GridList error={error} loading={loading} items={records}>
+        <Item />
+      </GridList>
+    </div>
+  );
 };
 
 export default Items;
--- src/state/index.js
+++ src/state/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categories from "./categorySlice";
+import items from "./ItemSlice";
 
-const store = configureStore({ reducer: { categories } });
+const store = configureStore({ reducer: { categories, items } });
 
 export default store;
