19308d9e47c8b892f1738981bdd396e8c6ff6066
kareem nour <kimzcodes@gmail.com>
2022-12-13 23:50:22 +0200

    remove console.log

--- src/state/CartSlice.js
+++ src/state/CartSlice.js
@@ -20,7 +20,6 @@ const cartSlice = createSlice({
 export const totalCartQuantity = createSelector(
   (state) => state.cart.items,
   (items) => {
-    console.log("fired");
     let totalQuantity = 0;
 
     for (const id in items) {
