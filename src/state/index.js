ed9431515b136f2d174b84102ed735a1f6c0241a
kareem nour <kimzcodes@gmail.com>
2022-12-28 22:01:48 +0200

    save

--- src/state/index.js
+++ src/state/index.js
@@ -1,8 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import categories from "./categorySlice";
 import items from "./ItemSlice";
 import cart from "./CartSlice";
 
-const store = configureStore({ reducer: { categories, items, cart } });
+const persistConfig = {
+  key: "ecom",
+  version: 1,
+  storage,
+  whitelist: ["cart"],
+};
 
-export default store;
+const rootReducer = combineReducers({ categories, items, cart });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
