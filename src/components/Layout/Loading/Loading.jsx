00930f1403c3069bd40edf2e57447de404c7d65c
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-19 18:38:55 +0200

    animations

--- src/components/Layout/Loading/Loading.jsx
+++ src/components/Layout/Loading/Loading.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import Lottie from "lottie-react";
+import shoppingCart from "../../../assets/lottie/shoppingCart.json";
+import connectionError from "../../../assets/lottie/connectionError.json";
+import styles from "./styles.module.css";
 
 const Loading = ({ children, loading, error }) => {
+  const { lottieAnimation } = styles;
   return (
-    <div>
-      {loading ? "Loading please wait" : error ? "Error from server" : children}
-    </div>
+    <>
+      {loading ? (
+        <div className={lottieAnimation}>
+          <Lottie animationData={shoppingCart} loop={true} />
+        </div>
+      ) : error ? (
+        <div className={lottieAnimation}>
+          <Lottie animationData={connectionError} loop={true} />
+          <h2>OOPs! Something went wrong</h2>
+        </div>
+      ) : (
+        children
+      )}
+    </>
   );
 };
 
