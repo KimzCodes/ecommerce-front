21ae6e6be3162e10b6a5459b0f7367074f4e0a32
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-20 17:57:44 +0200

    create

--- src/store/categories/asyncThunk.js
+++ src/store/categories/asyncThunk.js
@@ -1,12 +1,12 @@
-import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { customAPI } from "../../API/axios-custom";
 
 export const getCategories = createAsyncThunk(
   "categories/getCategories",
   async (_, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
     try {
-      const { data } = await axios.get("/category");
+      const { data } = await customAPI.get("/category");
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
