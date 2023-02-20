18e3e54d33a7a20b752e7f9db62b999d7a994d96
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-20 19:06:09 +0200

    create

--- src/API/axios-custom.js
+++ src/API/axios-custom.js
@@ -7,4 +7,18 @@ const customAPI = axios.create({
   },
 });
 
+customAPI.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log("cat");
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
 export { customAPI };
--- src/API/axios-global.js
+++ src/API/axios-global.js
@@ -1,7 +1,32 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5005";
+// axios.defaults.baseURL = "http://localhost:5005";
 axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  function (config) {
+    config.baseURL = "http://localhost:5005";
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 //global
 // axios.create({
