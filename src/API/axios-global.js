de7ed8d732299bc1a7cdcd7c81bbcdf8b80e8069
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-20 17:52:01 +0200

    add more

--- src/API/axios-global.js
+++ src/API/axios-global.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:5005";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 //global
 // axios.create({
 //   baseURL: "https://api.example.com",
-// });
-
-//custom
-//axios.create({
-//   baseURL: "https://api.example.com",
+//   header: {
+//     "Content-Type": "application/json",
+//   },
 // });
