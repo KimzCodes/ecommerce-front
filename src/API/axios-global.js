ff02b92ebb4ce871a255a24a4b717346bd48a450
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-22 18:34:13 +0200

    async await

--- src/API/axios-global.js
+++ src/API/axios-global.js
@@ -13,19 +13,23 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 
 // Add a response interceptor
 axios.interceptors.response.use(
-  function (response) {
+  (response) => {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
   },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    if (!error.config.url.includes("/tracking")) {
-      axios.post("http://localhost:50055/tracking", {
-        data: { type: "response", status: "failed" },
-      });
+  async (error) => {
+    try {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      if (!error.config.url.includes("/tracking")) {
+        await axios.post("http://localhost:50055/tracking", {
+          data: { type: error.config.url, status: "failed" },
+        });
+      }
+      return Promise.reject(error);
+    } catch (error) {
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
   }
 );
