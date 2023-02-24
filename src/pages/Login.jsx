960fd11d5743cbfe18b8c4d99c4441ecc7ece153
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-24 19:54:33 +0200

    validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,48 @@
 import { Button, Form, Row, Col } from "react-bootstrap";
+import { useFormik } from "formik";
+import { loginSchema } from "../util/validationSchema";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <Row className="justify-content-md-center">
       <Col xs={6} span={3}>
-        <Form>
+        <Form onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" />
+            <Form.Control
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              isInvalid={!!formik.errors.email}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.email}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              isInvalid={!!formik.errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.password}
+            </Form.Control.Feedback>
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,92 @@
 import { Button, Form, Row, Col } from "react-bootstrap";
+import { useFormik } from "formik";
+import { registerSchema } from "../util/validationSchema";
 
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+      passwordConf: "",
+    },
+    validationSchema: registerSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Row className="justify-content-md-center">
       <Col xs={6} span={3}>
-        <Form>
+        <Form onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3" controlId="fname">
             <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              name="fname"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.fname}
+              isInvalid={!!formik.errors.fname}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.fname}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="lname">
             <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              name="lname"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.lname}
+              isInvalid={!!formik.errors.lname}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.lname}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="email">
             <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" />
+            <Form.Control
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              isInvalid={!!formik.errors.email}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.email}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="password">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              isInvalid={!!formik.errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.password}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="confpassword">
+          <Form.Group className="mb-3" controlId="passwordConf">
             <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              name="passwordConf"
+              onChange={formik.handleChange}
+              value={formik.values.passwordConf}
+              isInvalid={!!formik.errors.passwordConf}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.passwordConf}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Button variant="primary" type="submit">
