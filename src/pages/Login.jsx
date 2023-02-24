612f1d7b13c0f280b1bd700c7e23ba77c042ca80
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-02-24 18:50:51 +0200

    create forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Row className="justify-content-md-center">
+      <Col xs={6} span={3}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Row className="justify-content-md-center">
+      <Col xs={6} span={3}>
+        <Form>
+          <Form.Group className="mb-3" controlId="fname">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="lname">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="confpassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  );
 };
 
 export default Register;
