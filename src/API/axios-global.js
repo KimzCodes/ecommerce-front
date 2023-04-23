719919c2c4a590c421b194afa124ff87dd7d9baf
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-04-23 22:17:44 +0200

    fixes

--- src/API/axios-global.js
+++ src/API/axios-global.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:50055";
+axios.defaults.baseURL = "http://localhost:5005";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
-// Add a request interceptor
 axios.interceptors.request.use(
   (config) => {
     // Do something before request is sent
@@ -18,20 +18,13 @@ axios.interceptors.request.use(
 );
 
 // Add a response interceptor
-axios.interceptors.re.use(
-  (response) => {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  async (error) => {
+axios.interceptors.response.use(null, async (error) => {
+  if (!error.config.url.includes("tracking") && !axios.isCancel(error)) {
     await axios.post("http://localhost:5006/tracking", {
       message: error.message,
       endPoint: error.config.url,
     });
-    if (!error.config.url.includes("tracking") && !axios.isCancel(error)) {
-    }
-
-    return Promise.reject(error);
   }
-);
+
+  return Promise.reject(error);
+});
