59ddd7ba6907bc44f05c91f0b61bed706ed5c93b
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-04-23 22:09:58 +0200

    fix

--- src/API/axios-global.js
+++ src/API/axios-global.js
@@ -2,8 +2,23 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:50055";
 
+// Add a request interceptor
+axios.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    if (config.url.includes("items")) {
+      config.url = `${config.url}&fireEvent=true`;
+    }
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 // Add a response interceptor
-axios.interceptors.response.use(
+axios.interceptors.re.use(
   (response) => {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
