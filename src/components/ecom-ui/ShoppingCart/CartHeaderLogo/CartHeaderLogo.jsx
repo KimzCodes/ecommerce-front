966a99ad50e2ab47f4d22aea810b6ed4deaa9ff3
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-05-21 14:28:33 +0300

    fix

--- src/components/ecom-ui/ShoppingCart/CartHeaderLogo/CartHeaderLogo.jsx
+++ src/components/ecom-ui/ShoppingCart/CartHeaderLogo/CartHeaderLogo.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef, useCallback } from "react";
 import { useSelector } from "react-redux";
-import { cartTotalQuantity } from "../../../../store/cartSlice";
+import { cartTotalQuantity } from "../../../../store/cart/cartSlice";
 import { CartDrop } from "../../";
 import shoppingCartImg from "../../../../assets/shopping-card.svg";
 import styles from "./styles.module.css";
