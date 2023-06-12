56d73496b742b07b817660b4666074b92bb59e7f
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-06-12 18:40:44 +0300

    fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
-              type="name"
+              type="text"
               name="email"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
