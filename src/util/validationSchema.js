44d17c3d6d13ea3210baf596cd4e249776defb63
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-06-20 22:06:32 +0300

    fix

--- src/util/validationSchema.js
+++ src/util/validationSchema.js
@@ -8,9 +8,19 @@ export const loginSchema = yup.object({
   password: yup.string("Enter your password").required("Password is required"),
 });
 
+const getCharacterValidationError = (str) => {
+  return `Your password must have at least 1 ${str} character`;
+};
+
 export const registerSchema = yup.object({
-  fname: yup.string().required("First name is required"),
-  lname: yup.string().required("Last name is required"),
+  firstName: yup
+    .string()
+    .required("First name is required")
+    .max(30, "First name should be maximum 30 characters"),
+  lastName: yup
+    .string()
+    .required("Last name is required")
+    .max(30, "Last name should be maximum 30 characters"),
   email: yup
     .string()
     .email("Enter a valid email")
@@ -18,6 +28,13 @@ export const registerSchema = yup.object({
   password: yup
     .string()
     .min(8, "Password should be of minimum 8 characters length")
+    .matches(/[0-9]/, getCharacterValidationError("digit"))
+    .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+    .matches(/[A-Z]/, getCharacterValidationError("uppercase"))
+    .matches(
+      /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/,
+      getCharacterValidationError("special symbol")
+    )
     .required("Password is required"),
   passwordConf: yup
     .string()
