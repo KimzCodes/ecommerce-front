4702a0392153916823ec7afd5f4230ea5f906b07
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-05 18:33:36 +0300

    fiox

--- src/pages/profile/Account.jsx
+++ src/pages/profile/Account.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import useAuth from "../../hooks/use-auth";
+
 const Account = () => {
-  return <div>Account</div>;
+  const { userInfo } = useAuth();
+  return (
+    <div>
+      <ul>
+        <li>First Name:{userInfo.firstName}</li>
+        <li>Last Name:{userInfo.lastName}</li>
+        <li>Email:{userInfo.email}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Account;
