e1c8d2d4c2486bd96b64098370a656fd8b9a0a44
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-20 18:00:12 +0300

    finish

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../store/categorySlice";
 
 const Categories = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
   return <div>Categories</div>;
 };
 
--- src/store/categorySlice.js
+++ src/store/categorySlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   records: [],
@@ -6,11 +6,39 @@ const initialState = {
   error: null,
 };
 
+export const getCategories = createAsyncThunk(
+  "category/getCategories",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await fetch("http://localhost:5005/categories");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
 const categorySlice = createSlice({
   name: "category",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCategories.fulfilled, (state, action) => {
+      state.loading = false;
+      state.records = action.payload;
+    });
+    builder.addCase(getCategories.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+  },
 });
 
 export default categorySlice.reducer;
