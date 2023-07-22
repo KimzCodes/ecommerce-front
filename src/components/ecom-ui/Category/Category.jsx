d25700be07d6d52d94c5e474d32c401e006fd7ff
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-22 17:06:10 +0300

    finish

--- src/components/ecom-ui/Category/Category.jsx
+++ src/components/ecom-ui/Category/Category.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
-const { category, categoryImg, categoryTitle } = styles;
+const { category, categoryImg, categoryTitle, categoryLink } = styles;
 
-const Category = ({ id, title, img }) => {
+const Category = ({ title, prefix, img }) => {
   return (
-    <div className={category}>
-      <div className={categoryImg}>
-        <img src={img} alt="" />
+    <Link to={`${prefix}/products`} className={categoryLink}>
+      <div className={category}>
+        <div className={categoryImg}>
+          <img src={img} alt={title} />
+        </div>
+        <h4 className={categoryTitle}>{title}</h4>
       </div>
-      <h4 className={categoryTitle}>{title}</h4>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,17 +2,27 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "../store/categorySlice";
 import { Category } from "../components/ecom-ui";
+
 const Categories = () => {
-  const { records } = useSelector((state) => state.category);
+  const { records, loading, error } = useSelector((state) => state.category);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCategories());
   }, [dispatch]);
 
-  const renderCategories = records.map((el) => (
-    <Category key={el.id} {...el} />
-  ));
+  const renderCategories =
+    records.length > 0
+      ? records.map((record) => <Category key={record.id} {...record} />)
+      : "There is no records available";
+
+  if (loading) {
+    return <div>Loading please wait</div>;
+  }
+
+  if (error) {
+    return <div>Error from server</div>;
+  }
 
   return <div className="grid">{renderCategories}</div>;
 };
