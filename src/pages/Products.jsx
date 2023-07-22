0c12d1d17d3625bc9cf5bbdf82f82508acfa6889
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-22 17:26:06 +0300

    products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterProducts } from "../store/productSlice";
+import { useParams } from "react-router-dom";
+import { Product } from "../components/ecom-ui";
 
 const Products = () => {
-  return <div>Products</div>;
+  const dispatch = useDispatch();
+  const { prefix } = useParams();
+
+  const { records, loading, error } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(filterProducts(prefix));
+  }, [prefix, dispatch]);
+
+  const renderProducts = records.map((record) => (
+    <Product key={record.id} {...record} />
+  ));
+
+  if (loading) {
+    return <div>Loading please wait</div>;
+  }
+
+  if (error) {
+    return <div>Error from server</div>;
+  }
+
+  return <div className="grid">{renderProducts}</div>;
 };
 
 export default Products;
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   records: [],
@@ -6,11 +6,41 @@ const initialState = {
   error: null,
 };
 
+export const filterProducts = createAsyncThunk(
+  "product/filterProducts",
+  async (catPrefix, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await fetch(
+        `http://localhost:5005/items?cat_prefix=${catPrefix}`
+      );
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      rejectWithValue(error.message);
+    }
+  }
+);
+
 const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(filterProducts.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(filterProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.records = action.payload;
+    });
+    builder.addCase(filterProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+  },
 });
 
 export default productSlice.reducer;
