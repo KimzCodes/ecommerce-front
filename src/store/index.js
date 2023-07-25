725f37113d5f9eb0b8ad3211b646dc2fcf20bf1a
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-25 19:42:41 +0300

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+// reducers
 import category from "./categorySlice";
 import product from "./productSlice";
 import cart from "./cartSlice";
 
-const store = configureStore({
-  reducer: { category, product, cart },
+const rootPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  category,
+  product,
+  cart,
 });
 
-export default store;
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
