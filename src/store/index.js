a12ff08cacc397990a47bd5a195056080753be49
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-07-27 02:01:24 +0300

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// reducers
 import category from "./categorySlice";
 import product from "./productSlice";
 import cart from "./cartSlice";
@@ -10,19 +18,31 @@ import cart from "./cartSlice";
 const rootPersistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart"],
+  whitelist: [],
+};
+
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+  whitelist: ["items"],
 };
 
 const rootReducer = combineReducers({
   category,
   product,
-  cart,
+  cart: persistReducer(cartPersistConfig, cart),
 });
 
 const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
