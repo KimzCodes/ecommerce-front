ce56e9714244da22ea803176835a9f7546489e5d
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-08-19 15:32:53 +0300

    finish

--- src/components/layout/Notifications/NotificationItem.jsx
+++ src/components/layout/Notifications/NotificationItem.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import { removeToast } from "../../../store/toastSlice";
 import { useDispatch } from "react-redux";
@@ -10,7 +10,13 @@ const { notification, indicator } = styles;
 
 const NotificationItem = ({ id, title, description, type }) => {
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(0);
+
+  const [progress, setProgress] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const autoCloseDuration = useRef(5000);
+  const autoCloseSetTimeOut = useRef(null);
+  const progressSetInterval = useRef(null);
 
   const closeHandler = useCallback(
     (id) => {
@@ -20,25 +26,44 @@ const NotificationItem = ({ id, title, description, type }) => {
   );
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setLoading((prev) => {
-        const newValue = prev + 1;
-        if (newValue === 100) {
-          clearInterval(interval);
-        }
+    if (!isHovering) {
+      progressSetInterval.current = setInterval(() => {
+        setProgress((prev) => {
+          const newValue = prev + 1;
+          if (newValue === 100) {
+            clearInterval(progressSetInterval.current);
+          }
 
-        return newValue;
-      });
-    }, 49);
-  }, []);
+          return newValue;
+        });
+      }, 100);
+    }
+  }, [isHovering]);
 
   useEffect(() => {
-    const autoClose = setTimeout(() => {
-      closeHandler(id);
-    }, 5000);
+    if (!isHovering) {
+      autoCloseSetTimeOut.current = setTimeout(() => {
+        closeHandler(id);
+      }, autoCloseDuration.current);
 
-    return () => clearTimeout(autoClose);
-  }, [closeHandler, id]);
+      return () => clearTimeout(autoCloseSetTimeOut.current);
+    }
+  }, [closeHandler, id, isHovering]);
+
+  function handleMouseEnter(e) {
+    setIsHovering(true);
+
+    if (autoCloseSetTimeOut.current) {
+      clearTimeout(autoCloseSetTimeOut.current);
+      clearInterval(progressSetInterval.current);
+    }
+  }
+  function handleMouseLeave(e) {
+    setIsHovering(false);
+    const expiredTime = (autoCloseDuration.current * progress) / 100;
+    const remainingTime = autoCloseDuration.current - expiredTime;
+    autoCloseDuration.current = Math.trunc(remainingTime);
+  }
 
   return (
     <motion.div
@@ -46,12 +71,14 @@ const NotificationItem = ({ id, title, description, type }) => {
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: -200, opacity: 0 }}
       className={`alert alert-${type} ${notification}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <h6>{title}</h6>
       <p>{description}</p>
       <button className="btn-close" onClick={() => closeHandler(id)}></button>
 
-      <div className={indicator} style={{ width: `${loading}%` }}>
+      <div className={indicator} style={{ width: `${progress}%` }}>
         <Placeholder xs={12} bg={type} />
       </div>
     </motion.div>
